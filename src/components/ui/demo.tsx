2d1d70e87237752f885cf46cdaff8cf0f411cd7f
Ritik Pathak <ritikpathak6464@gmail.com>
2026-09-12 03:29:48 +0530

    Add animated hero demo component

---
+++ src/components/ui/demo.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/ui/animated-hero";
+
+function HeroDemo() {
+  return (
+    <div className="block bg-[#FBF7EF] p-8">
+      <Hero />
+    </div>
+  );
+}
+
+export { HeroDemo };
