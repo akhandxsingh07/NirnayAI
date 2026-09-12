95b8c6560ff25a758093c5bd684537e933689d59
Ritik Pathak <ritikpathak6464@gmail.com>
2026-09-12 10:24:31 +0530

    Type Supabase client environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_NIRNAY_ADMIN_EMAIL?: string;
-  readonly VITE_NIRNAY_ADMIN_PASSCODE?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
 }
 
 interface ImportMeta {
